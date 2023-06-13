5918d649451358a8c647de4364a898c730a646e0
vladimir <vshalakhin@electro.cars>
2023-06-13 13:08:20 +0300

    Убрал ненужные комментарии

--- counter.php
+++ counter.php
@@ -1,15 +1,12 @@
 <?php
 require_once 'backend/sdbh.php';
 $dbh = new sdbh();
-//echo ($_POST['data'][0] * $_POST['data'][1]);
 preg_match_all('(\d+)', $_POST['data'][2], $matches);
-//$priceAdditional = $matches[0];
 $priceAdditional = 0;
 foreach ($matches[0] as $value) {
     $priceAdditional += $value * $_POST['data'][1];
 }
 $days = unserialize($dbh->mselect_rows('a25_products', ['ID' => $_POST['data'][0]], 0, 1, 'id')[0]['TARIFF']);
-    //отлов ошибок и если кол-во пришла пустота
     if ($days) {
         $price = 0;
         foreach ($days as $key => $value) {
